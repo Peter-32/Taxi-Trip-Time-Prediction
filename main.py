258203375da9fc03bfcd9535af25f24aa2f95473
Peter-32 <peter@impactradius.com>
2018-10-24 23:19:48 -0700

    a

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from pandasql import sqldf
-import time
+from scipy import spatial
 import re
 #pd.options.display.html.table_schema = True
 #pd.options.display.max_rows = None
@@ -23,10 +23,10 @@ class ETL:
             path = "/Users/peterjmyers/work/Taxi-Trip-Time-Prediction/data/"
         elif self.user == "cheryl":
             path = "/Users/cheryljose/Documents/Projects/all/"
-        self.df = pd.read_csv('{}train.csv'.format(path), sep=",", nrows=10)
+        self.df = pd.read_csv('{}train.csv'.format(path), sep=",", nrows=30)
         self.metadata_df = pd.read_csv('{}metaData_taxistandsID_name_GPSlocation.csv'.format(path), sep=",")
-        self.train_df = pd.read_csv('{}train.csv'.format(path), sep=",", nrows=10)
-        self.test_df = pd.read_csv('{}test.csv'.format(path), sep=",", nrows=10)
+        self.train_df = pd.read_csv('{}train.csv'.format(path), sep=",", nrows=30)
+        self.test_df = pd.read_csv('{}test.csv'.format(path), sep=",", nrows=30)
 
     def set_time_taken(self):
         self.df['polyline_length'] = self.df['POLYLINE'].apply(lambda x: len(x.split("],[")))
@@ -37,58 +37,71 @@ class ETL:
         self.metadata_df['Latitude'][40] = 41.163066654
         self.metadata_df['Longitude'][40] = -8.67598304213
         self.metadata_df.iloc[39:41]
-
+1+1
 etl = ETL("peter")
 etl = ETL("cheryl")
 etl.execute()
 df, metadata_df = etl.df, etl.metadata_df
 
+coordinates = metadata_df.values[:,2:].astype(float)
+locations = metadata_df.values[:,1]
+coordinates
+locations
+coordinates.shape
+locations.shape
+tree = spatial.KDTree(coordinates)
+
+#Running in a different notebook
+#you here ?
+
 def closest_location(longitude, latitude):
-    df_temp = q("select a.*, ((longitude - {}) * (longitude - {})) + ((latitude - {}) * (latitude - {})) d_squared from metadata_df a".format(longitude, longitude,  latitude, latitude))
-    df_temp.loc[:, 'd'] = df_temp['d_squared'].apply(lambda x: np.sqrt(x))
-    return sqldf("select Descricao from df_temp order by d asc limit 1", locals()).Descricao[0]
-
-# TODO: Fix closest location to use this code: (load the tree once and save it outside this function)
-https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.spatial.KDTree.html
-# >>> from scipy import spatial
-# >>> airports = [(10,10),(20,20),(30,30),(40,40)]
-# >>> tree = spatial.KDTree(airports)
-# >>> tree.query([(21,21)])
-# (array([ 1.41421356]), array([1]))
-that will do it
-
+    return locations[tree.query([(longitude, latitude)])[1][0]]
 
 closest_location(-8.618643,41.141412)
 
-j = 0
+def closest_location2(longitude, latitude):
+    return locations[tree.query([(longitude, latitude)])[1][0]]
+#df_temp = q("select a.*, ((longitude - {}) * (longitude - {})) + ((latitude - {}) * (latitude - {})) d_squared from metadata_df a".format(longitude, longitude,  latitude, latitude))
+#df_temp.loc[:, 'd'] = df_temp['d_squared'].apply(lambda x: np.sqrt(x))
+#sqldf("select Descricao from df_temp order by d asc limit 1", locals()).Descricao[0]
+
+closest_location2(-8.618643,41.141412)
+
+
+# Closest locations as column polyline_names
+
+# 1) Populate the list polyline_names_list
+
+
 polyline_names_list = []
 for i in range(df.shape[0]):
     polyline_names = []
     for coordinates in df.iloc[i].POLYLINE.split("],["):
         coordinates = coordinates.replace("[", "").replace("]", "")
-
         x, y = coordinates.split(",")[0], coordinates.split(",")[1]
         polyline_names.append(closest_location(x, y))
-        # j += 1
-    # polyline_names_list.append(polyline_names)
-    print(i)
-
-
-
-j=332
-#df.loc[:, 'd'] = polyline_names_list
-#df.head()
-
-# can probably speed it up at some point.  maybe closest_location
+    polyline_names_list.append(polyline_names)
+    print("a")
+df.loc[:, 'd'] = polyline_names_list
+df.head()
 
 
 
 
-https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.spatial.KDTree.html
-# >>> from scipy import spatial
-# >>> airports = [(10,10),(20,20),(30,30),(40,40)]
-# >>> tree = spatial.KDTree(airports)
-# >>> tree.query([(21,21)])
-# (array([ 1.41421356]), array([1]))
-# Where 1.41421356 is the distance between the queried point
-# the nearest neighbour and 1 is the index of the neighbour.
+
+
+We can add the code below `closest_location`:
+
+            https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.spatial.KDTree.html
+            # >>> from scipy import spatial
+            # >>> airports = [(10,10),(20,20),(30,30),(40,40)]
+            # >>> tree = spatial.KDTree(airports)
+            # >>>
+            # (array([ 1.41421356]), array([1]))
+
+
+
+
+#df_temp = q("select a.*, ((longitude - {}) * (longitude - {})) + ((latitude - {}) * (latitude - {})) d_squared from metadata_df a".format(longitude, longitude,  latitude, latitude))
+#df_temp.loc[:, 'd'] = df_temp['d_squared'].apply(lambda x: np.sqrt(x))
+#sqldf("select Descricao from df_temp order by d asc limit 1", locals()).Descricao[0]
